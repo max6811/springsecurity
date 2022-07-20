f1d6f3e52cff0cbac0831c2e25797de7a2464566
max6811 <maxc.m6811@gmail.com>
2022-07-19 23:03:47 -0400

    database authentication implemented

---
+++ src/main/java/maxcmartinez/com/springsecurity/auth/ApplicationUserDao.java
@@ -0,0 +1,7 @@
+package maxcmartinez.com.springsecurity.auth;
+
+import java.util.Optional;
+
+public interface ApplicationUserDao {
+    Optional<ApplicationUser> selectApplicationUserByUsername(String username);
+}
